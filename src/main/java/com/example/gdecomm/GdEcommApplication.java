3e81eefaf17ceb76fdbd0bec373539dc1ef0cb1e
wbsxAllen <wbsxallen@gmail.com>
2025-04-29 19:14:39 -0600

    initial

---
+++ src/main/java/com/example/gdecomm/GdEcommApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdEcommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdEcommApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gdecomm/GdEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdEcommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
